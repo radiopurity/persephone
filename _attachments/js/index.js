55eb4722ccb6570420001630196f0f178d213616
James Loach <james.loach@gmail.com>
2013-04-03 14:35:47 -0600

    Minor fixes to disclaimers

--- _attachments/js/index.js
+++ _attachments/js/index.js
@@ -221,10 +221,10 @@ $(function(){
    
   $('span#disclaimers').click(function(e) {      
     $('div#disclaimer').fadeToggle();
-    if ( $('a#disclaimers').text() == text_1 ) { 
-      $('a#disclaimers').text(text_2);     
+    if ( $('span#disclaimers').text() == text_1 ) { 
+      $('span#disclaimers').text(text_2);     
     } else {
-      $('a#disclaimers').text(text_1);   
+      $('span#disclaimers').text(text_1);   
     }  
     e.preventDefault();    
   });
