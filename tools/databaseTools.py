6ef135a31d4c43d8106b7f333e8db33ca0848e9f
James Loach <james.loach@gmail.com>
2013-07-15 19:19:14 +0800

    Convert tabs to spaces

--- tools/databaseTools.py
+++ tools/databaseTools.py
@@ -43,7 +43,7 @@ import requests
 import json
 import codecs
 
-name = "RadioDBTools.py"
+name        = "RadioDBTools.py"
 MADFversion = "2.01"
 
 # .............................................................................
@@ -54,11 +54,11 @@ def help():
     print ("\nUsage: python" + name + "[-u|-d|-h]")
     print ("\n\nOptions:\n")
     print ("-u : Uploads .json files to a couchdb instance of your choice.\n")
-    print ("	 Useage: python" + name + "-u.\n")
+    print ("     Useage: python" + name + "-u.\n")
     print ("-d : Downloads .json files from a couchdb instance of your choice.\n")
-    print ("	 Useage: python" + name + "-d.\n")
+    print ("     Useage: python" + name + "-d.\n")
     print ("-p : Prunes (Deletes) all files of type \"measurement\" from a couchdb instance of your choice.\n")
-    print ("	 Useage: python" + name + "-p.\n")
+    print ("     Useage: python" + name + "-p.\n")
 
 # .............................................................................
 # Calls the different subroutines based on command line arguments
@@ -66,32 +66,33 @@ def main():
 
     if len(sys.argv) > 1 :
 
-    	if sys.argv[1] == "-h":
-       	    help()
-    	elif sys.argv[1] == "-u":
-    	    if len(sys.argv) == 2:
-       		upload_json()
-       	    else:
-       	        help()
-       	        print ("\n*******\nERROR:\nIncorrect number of arguments.\n*******\n")
-    	elif sys.argv[1] == "-d":
-    	    if len(sys.argv) == 2:
-    	        download_json()
-    	    else:
-    	        help()
-    		print ("\n*******\nERROR:\nIncorrect number of arguments.\n*******\n")
-    	elif sys.argv[1] == "-p":
-    	    if len(sys.argv) == 2:
-    	        prune_db()
-    	    else:
-    	        help()
-    	        print ("\n*******\nERROR:\nIncorrect number of arguments.\n*******\n")
-    	else:
-    	    help()
-    	    print("\n*******\nERROR:\nUnknown or incorrect argument. Could not parse <" + sys.argv[1] + ">. \nPlease try again.\n*******\n")
+        if sys.argv[1] == "-h":
+            help()
+        elif sys.argv[1] == "-u":
+            if len(sys.argv) == 2:
+                upload_json()
+            else:
+                help()
+                print ("\n*******\nERROR:\nIncorrect number of arguments.\n*******\n")
+        elif sys.argv[1] == "-d":
+            if len(sys.argv) == 2:
+                download_json()
+            else:
+                help()
+                print ("\n*******\nERROR:\nIncorrect number of arguments.\n*******\n")
+        elif sys.argv[1] == "-p":
+            if len(sys.argv) == 2:
+                prune_db()
+            else:
+                help()
+                print ("\n*******\nERROR:\nIncorrect number of arguments.\n*******\n")
+        else:
+            help()
+            print("\n*******\nERROR:\nUnknown or incorrect argument. Could not parse <" \
+                  + sys.argv[1] + ">. \nPlease try again.\n*******\n")
     else:
-    	help()
-    	print("\n*******\nERROR\nNo argument specified.\nPlease try again.\n*******\n")
+        help()
+        print("\n*******\nERROR\nNo argument specified.\nPlease try again.\n*******\n")
 
 # .............................................................................
 def get_uuid(db):
@@ -99,7 +100,7 @@ def get_uuid(db):
     import requests
     uuid_url = db + "/_uuids"
     uuid = requests.get(uuid_url).text[11:][:-4]
-	
+    
     return uuid
 
 # .............................................................................
@@ -111,16 +112,16 @@ def upload_json():
     uploadAll = raw_input(msg).lower()
 
     if uploadAll == "y":
-    	uploadListing = os.listdir(pwd)
+        uploadListing = os.listdir(pwd)
     else:
-    	print("\n")
-    	uploadListing =[]
-    	dirListing = os.listdir(pwd)
-    	for i in dirListing:
-       	    msg = "Would you like to upload " + i + " (Y/N) ? "
-    	    upload = raw_input(msg).lower()
-    	    if upload== "y":
-    	    	uploadListing.append(i)
+        print("\n")
+        uploadListing =[]
+        dirListing = os.listdir(pwd)
+        for i in dirListing:
+            msg = "Would you like to upload " + i + " (Y/N) ? "
+            upload = raw_input(msg).lower()
+            if upload== "y":
+                uploadListing.append(i)
 
     couchdb = raw_input("CouchDB URL (no username or password, enter for localhost) : ")
     if len(couchdb) < 3: couchdb = "http://localhost:5984"
@@ -135,22 +136,22 @@ def upload_json():
     
     print ("\n Document Name           HTML Status Code (201 is Success)")
     for i in uploadListing:
-    	f = open ( i, 'r')
-    	data = f.read()
-    	doc_url = couchdb + "/" + db + "/" + get_uuid(couchdb)
+        f = open ( i, 'r')
+        data = f.read()
+        doc_url = couchdb + "/" + db + "/" + get_uuid(couchdb)
         r = requests.put(doc_url, data = data, auth = (username, password))
-    	print(' ' + i.ljust(31) + repr(r).rjust(26))
-    	if r.status_code == requests.codes.created:
-    	    successes.append(i)
-    	else:
-    	    failures.append(i)
+        print(' ' + i.ljust(31) + repr(r).rjust(26))
+        if r.status_code == requests.codes.created:
+            successes.append(i)
+        else:
+            failures.append(i)
     
     print ("\nSuccessful Uploads : " + repr(len(successes)))
     print ("Failed Uploads     : " + repr(len(failures)))
     if len(failures) > 0:
-    	print ("\nThese files failed to upload:")
-    	for i in failures:
-    	    print ("   " + i)
+        print ("\nThese files failed to upload:")
+        for i in failures:
+            print ("   " + i)
     print ''
 
 # .............................................................................
@@ -206,23 +207,23 @@ def prune_db():
     doc_ids = []
     doc_ids_raw = re.findall("\"id\":\".*?\"", all_docs.text)
     for i in doc_ids_raw:
-    	doc_ids.append(i[6:-1])
+        doc_ids.append(i[6:-1])
 
     count = 0
     del_count = 0
     del_ids = []
     for i in doc_ids:
-    	count += 1
-    	doc_url = db_url + i
+        count += 1
+        doc_url = db_url + i
         r_raw = requests.get(doc_url, auth=(username, password))
-    	r_raw2 = re.sub("\" ?date ?\" ?: ?\[.*\]","", r_raw.text , re.DOTALL)
-    	r = re.sub("\" ?results ?\" ?: ?\[.*\]","", r_raw2 , re.DOTALL)
-    	match = None
-    	match = re.search( "\" ?type ?\" ?: ?\" ?measurement ?\" ?" ,r)
-    	if match:
-    	    del_ids.append(i)
-    	    del_count += 1
-    	if count % 10 == 0:
+        r_raw2 = re.sub("\" ?date ?\" ?: ?\[.*\]","", r_raw.text , re.DOTALL)
+        r = re.sub("\" ?results ?\" ?: ?\[.*\]","", r_raw2 , re.DOTALL)
+        match = None
+        match = re.search( "\" ?type ?\" ?: ?\" ?measurement ?\" ?" ,r)
+        if match:
+            del_ids.append(i)
+            del_count += 1
+        if count % 10 == 0:
             print(" searching ... " + repr(count).rjust(4) + " / " + repr(len(doc_ids)).rjust(4))
 
     print ("\nNumber of documents to delete: " + repr(len(del_ids)))
@@ -231,25 +232,25 @@ def prune_db():
     count = 0
 
     for i in del_ids:
-    	count += 1
-    	doc_url = db_url + i
-    	rev_raw = requests.get( doc_url , auth=( username, password))
-    	rev_array = re.findall("\"_?rev\":\".*?\"", rev_raw.text)
-    	rev = rev_array[0][7:].replace("\"","")
-    	del_url = db_url + i + "?rev=" + rev
-    	r = requests.delete(del_url, auth=(username,password))
-    	if count % 10 == 0:
+        count += 1
+        doc_url = db_url + i
+        rev_raw = requests.get( doc_url , auth=( username, password))
+        rev_array = re.findall("\"_?rev\":\".*?\"", rev_raw.text)
+        rev = rev_array[0][7:].replace("\"","")
+        del_url = db_url + i + "?rev=" + rev
+        r = requests.delete(del_url, auth=(username,password))
+        if count % 10 == 0:
             print(" pruning ... " + repr(count).rjust(4) + " / " + repr(len(doc_ids)).rjust(4))
-    	if r.status_code == requests.codes.ok:
-    	    successes.append(i)
-    	else:
-    	    failures.append(i)
+        if r.status_code == requests.codes.ok:
+            successes.append(i)
+        else:
+            failures.append(i)
 
     if len(failures) > 0:
-    	print ("\n\nThese files were not deleted :")
-    	for i in failures:
-    		print ("\t" + i)
-    	print ("\n\n")
+        print ("\n\nThese files were not deleted :")
+        for i in failures:
+            print ("\t" + i)
+        print ("\n\n")
  
     print ''
     print ("Documents searched                                 : " + repr(len(doc_ids)).rjust(5))
@@ -261,4 +262,4 @@ def prune_db():
 # .............................................................................
 # Allows execution as a script or as a module
 if __name__ == '__main__':
-	main()
+    main()
