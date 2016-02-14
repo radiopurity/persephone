222d6d4e798eb7a153dc6019e5926c2d87e18c51
James Loach <james.loach@gmail.com>
2016-02-14 08:45:54 +0000

    Regularize formatting

--- tools/removeSearchIndice.py
+++ tools/removeSearchIndice.py
@@ -1,85 +1,55 @@
 #!/usr/bin/python
-
-# ============
-# generateSearchIndice.py
-# ============
-#
-#    --------------------------------------------------------------------------
-#    Name:           generateSearchIndice.py
-#    Purpose:        Remove the search indices of Uranium and Thorium for Lucene search.
-#         
-#    Author:         Zheng Li
-#    Email:          ronnie.alonso@gmail.com
-#
-#    Created:        23/07/2013
-#    Copyright:      (c) Zheng Li 2013
-#    Licence:        GNU General Public License
-#    Version:        1.2
-#    --------------------------------------------------------------------------
-
-import os , json , glob , string , sys
 """
-Modify the JSON type to satisfy the json schema.
-
-Arguments:
-    doc : the input measurement JSON
+Remove serach indicies
 """
+
+
+import os, json, glob, string, sys
+
+
 def modifySchemaType(doc):
+  """ Clean up JSON """
+  # Modify measurement results to change all the value to a float
+  if (doc["measurement"].has_key("results")):
+    for i in range(len(doc["measurement"]["results"])):
+      for j in range(len(doc["measurement"]["results"][i]["value"])):
 
-    """ Modify measurement results to change all the value to a float"""
-    if (doc["measurement"].has_key("results")):
-        for i in range(len(doc["measurement"]["results"])):
-            for j in range(len(doc["measurement"]["results"][i]["value"])):
+        if isinstance(doc["measurement"]["results"][i]["value"][j], (str, unicode)):
+          doc["measurement"]["results"][i]["value"][j] =  float(doc["measurement"]["results"][i]["value"][j])
+  # Replace the date entry which has value of "" to value of []
+  if (doc["measurement"].has_key("date")):
+    if isinstance(doc["measurement"]["date"], (str , unicode)):
+      doc["measurement"]["date"] = []
+  # Remove the _rev and _id fields
+  if (doc.has_key("_rev")):
+    del doc["_rev"]
+  if (doc.has_key("_id")):
+    del doc["_id"]
+  return doc
 
-                if isinstance(doc["measurement"]["results"][i]["value"][j], (str , unicode)):
-                    doc["measurement"]["results"][i]["value"][j] =  float(doc["measurement"]["results"][i]["value"][j])
 
-    """ Replace the date entry which has value of "" to value of [] """
-    if (doc["measurement"].has_key("date")):
-
-        if isinstance(doc["measurement"]["date"], (str , unicode)):
-            doc["measurement"]["date"] = []
-
-    """ Remove the _rev entry to avoid CouchDB 409 error """
-    if(doc.has_key("_rev")):
-        del doc["_rev"]
-    if(doc.has_key("_id")):
-        del doc["_id"]
-
-    return doc
-
-"""
-Remove search indices for isotopes: Uranium and Thorium.
-
-Arguments:
-    doc : the input measurement JSON
-"""
 def removeSearchIndice(doc):
-    if(doc.has_key("sort_indices")):
-        del doc["sort_indices"]
-    return doc
+  """ Remove search indices for isotopes Uranium and Thorium """
+  if (doc.has_key("sort_indices")):
+    del doc["sort_indices"]
+  return doc
 
-# _dir_ : "/mychr"
-# _type_ : "*.json"
-def openFile(_dir_ , _type_):
-    os.chdir(_dir_)
-    print "Filename\t\t\t\tStatus"
-    for filename in glob.glob(_type_):
-        json_file = open(filename)
-        json_data = json.load(json_file)
 
-        json_data = modifySchemaType(json_data)
+def openFile(_dir_, _type_):
+  """ Process file """
+  os.chdir(_dir_)
+  print "Filename\t\t\t\tStatus"
+  for filename in glob.glob(_type_):
+    json_file = open(filename)
+    json_data = json.load(json_file)
+    json_data = modifySchemaType(json_data)
+    json_data = removeSearchIndice(json_data)
+    json_file.close()
+    json_file = open(filename,"w")
+    json_file.write(json.dumps(json_data, indent=2))
+    json_file.close()
+    print filename + "\t\t\t\tCompleted"
 
-        json_data = removeSearchIndice(json_data)
-
-        json_file.close()
-
-        json_file = open(filename,"w")
-        json_file.write(json.dumps(json_data, indent=4))
-
-        json_file.close()
-
-        print filename + "\t\t\t\tCompleted"
 
 if __name__ == '__main__':
     """Calls the different subroutines based on command line arguments"""
