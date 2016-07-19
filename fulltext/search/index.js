7479ae7eebfd1ef871c81667de0cbd5236675ebb
James Loach <james.loach@gmail.com>
2016-07-19 13:37:34 +0800

    Cleanup

--- fulltext/search/index.js
+++ fulltext/search/index.js
@@ -10,55 +10,44 @@ function(doc) {
     ret.add(doc.sample.name, {"field":"name", "store":"yes"});
     ret.add(doc.sample.description, {"field":"description", "store":"yes"});
   };
-  if (doc.sort_indices) {
-    ret.add(doc.sort_indices.th);
-  };
-  if (doc.sort_indices) {
-    ret.add(doc.sort_indices.u);
-  };
   if (doc.measurement) {
-  if (doc.measurement.results) {
-  if (doc.measurement.results.length > 0) {
-    for (var i=0; i<doc.measurement.results.length; i++) {
-      var value = doc.measurement.results[i];
-      var result1 = "", result2 = "";
-      if (value.type == "measurement") {
-        if (value.value.length == 1) {
-          result1 = value.value[0];
-        } else if (value.value.length == 2) { result1 = value.value[0];
-           result2 = "(" + value.value[1] + ")";
-        } else if (value.value.length == 3) {
-           result1 = value.value[0];
-           result2 = "(" + value.value[1] + "-" + value.value[2] + ")";
-        }
-      } else if (value.type == "limit") {
-        if (value.value.length == 1) {
-          result1 = value.value[0];
-        } else if (value.value.length == 2) {
-          result1 = value.value[0];
-          result2 = "(" + value.value[1] + "%)";
-        }
-      } else if (value.type == "range") {
-        if (value.value.length == 2) {
-          result1 = value.value[0] + " - " + value.value[1];
-        } else if (value.value.length == 3) {
-          result1 = value.value[0] + " - " + value.value[1];
-          result2 = "(" + value.value[2] + "%)";
+    if (doc.measurement.results) {
+      if (doc.measurement.results.length > 0) {
+        for (var i=0; i<doc.measurement.results.length; i++) {
+          var value = doc.measurement.results[i];
+          var result1 = "", result2 = "";
+          if (value.type == "measurement") {
+            if (value.value.length == 1) {
+              result1 = value.value[0];
+            } else if (value.value.length == 2) { result1 = value.value[0];
+               result2 = "(" + value.value[1] + ")";
+            } else if (value.value.length == 3) {
+               result1 = value.value[0];
+               result2 = "(" + value.value[1] + "-" + value.value[2] + ")";
+            }
+          } else if (value.type == "limit") {
+            if (value.value.length == 1) {
+              result1 = value.value[0];
+            } else if (value.value.length == 2) {
+              result1 = value.value[0];
+              result2 = "(" + value.value[1] + "%)";
+            }
+          } else if (value.type == "range") {
+            if (value.value.length == 2) {
+              result1 = value.value[0] + " - " + value.value[1];
+            } else if (value.value.length == 3) {
+              result1 = value.value[0] + " - " + value.value[1];
+              result2 = "(" + value.value[2] + "%)";
+            }
+          }
+          ret.add(value.isotope, {"field":"isotope", "store":"yes"});
+          ret.add(value.unit, {"field":"unit", "store":"yes"});
+          ret.add(value.type, {"field":"type", "store":"yes"});
+          ret.add(result1, {"field":"result1", "store":"yes"});
+          ret.add(result2, {"field":"result2", "store":"yes"});
         }
       }
-      ret.add(value.isotope, {"field":"isotope", "store":"yes"});
-      ret.add(value.unit, {"field":"unit", "store":"yes"});
-      ret.add(value.type, {"field":"type", "store":"yes"});
-      ret.add(result1, {"field":"result1", "store":"yes"});
-      ret.add(result2, {"field":"result2", "store":"yes"});
     }
   }
-  }
-  }
-  //index("isotope", "", {"store":"yes"});
-  //index("unit", "", {"store":"yes"});
-  //index("type", "", {"store":"yes"});
-  //index("result1", "", {"store":"yes"});
-  //index("result2","",{"store":"yes"});  
   return ret;
 }
